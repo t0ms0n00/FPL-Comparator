f8c221a7834be8badf0c7f8741a1b0cb87160008
Tomasz <tomaszboron2k@gmail.com>
2022-03-20 10:30:00 +0100

    check more clubs with video api

--- apis/videoAPI.py
+++ apis/videoAPI.py
@@ -9,22 +9,22 @@ class PLVideoAPI:
         self.token = 'MTU0MjFfMTY0NzE2NTg5MV8zNzQ0YmNhMTg2ZjBlMjAxMTIyOWM2OTgyNjdlNjQwZWEzYmQ0ZWUy'
         self.nameMap = {
             'Arsenal': 'Arsenal',
-            'Aston Villa': 'Aston Villa', #check
+            'Aston Villa': 'Aston Villa',
             'Brentford': 'Brentford',
             'Brighton': 'Brighton',
             'Burnley': 'Burnley',
             'Chelsea': 'Chelsea',
-            'Crystal Palace': 'Crystal Palace', #check
+            'Crystal Palace': 'Crystal Palace',
             'Everton': 'Everton',
-            'Leicester': 'Leicester',   # check
+            'Leicester': 'Leicester',
             'Leeds': 'Leeds',   # check
             'Liverpool': 'Liverpool',
             'Man City': 'Manchester City',
             'Man Utd': 'Manchester United',
-            'Newcastle': 'Newcastle', #check
+            'Newcastle': 'Newcastle',
             'Norwich': 'Norwich',
             'Southampton': 'Southampton', #check
-            'Spurs': 'Tottenham Hotspur', #check
+            'Spurs': 'Tottenham Hotspur',
             'Watford': 'Watford',
             'West Ham': 'West Ham',
             'Wolves': 'Wolves' #check
